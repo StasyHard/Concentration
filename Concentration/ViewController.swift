f37c77a080c7c60f6d843a65757852b22d0d2faf
StasyHard <reyngardt.as@yandex.ru>
2019-10-06 23:23:23 +0300

    moved FlipCount to Concentration

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -21,14 +21,8 @@ class ViewController: UIViewController {
     static let defaultEmojies = [ "👻", "😱", "🤐", "👽", "😈", "🎃", "🙀", "👾", "👺" ]
     var emojiChoices: [String] = defaultEmojies
     
-    var flipCount = 0 {
-        didSet {
-            flipCountLabel.text = "Flips: \(flipCount)"
-        }
-    }
     
     @IBAction func touchCard(_ sender: UIButton) {
-        flipCount += 1
         if let cardNumber = cardButtons.firstIndex(of: sender) {
             game.chooseCard(at: cardNumber)
             if game.isGameOver() {
@@ -52,6 +46,7 @@ class ViewController: UIViewController {
                 button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
             }
         }
+        flipCountLabel.text = "Flips: \(game.flipCount)"
     }
 
     var emoji = [Int: String]()
